1ffa8d71b120c3b336f618df911e534b1e4e613a
Gilbert Andanje <gilbertandanje@gmail.com>
2024-03-10 00:41:20 +0300

    feat: handle prod & dev errors & handle op & non-operational errors

--- userService/src/errors/errors.handler.js
+++ userService/src/errors/errors.handler.js
@@ -1,23 +1,58 @@
-const catchAsync = (fn) =>(req,res,next) =>{
-    return Promise.resolve(fn(req,res,next)).catch((err)=>{
-        next(err)
-    })
-}
-    
-const errorHandler = (err,req,res,next) => {
-// TODO: create the global error handler
-let message;
-let statutsCode;
-    switch(err.code){
-        case 11000:
-        message = `A user with existing email exists: ${req.body.email}`
-        statutsCode = 400
+const config = require("../config/config");
+const { logger } = require("../config/logger");
+const { APIError } = require("../utilities/APIError");
+
+const prodErrors = (err, res) => {
+    if (err.isOperational) {
+        res.status(err.statusCode).json({
+            status: err.status,
+            message: err.message
+        });
+    } else {
+        res.status(500).json({
+            status: 'error',
+            message: 'Something wrong happened! Try again later'
+        });
     }
-    console.log(err)
-    res.status(statutsCode).json({error:message})
+}
+const devErrors = (err, res) => {
+    res.status(err.statusCode).json({
+        status: err.statusCode,
+        message: err.message,
+        stack: err.stack,
+        err
+    });
+}
+const castErrorHandler = (err) => {
+    err.message = `Invalid value ${err.value} for field ${err.path}`
+    return new APIError(400,err.message)
+}
+const duplicateKeyErrorHandler = (err) => {
+    const key = Object.keys(err.keyValue)[0]
+    const value = err.keyValue[key]
+    const message = `Entry with ${key}:${value} already exists`
+    return new APIError(400,message)
+}
+const errorHandler = (err, req, res, next) => {
+    // TODO: create the global error handler
+    err.message = err.message || 'Internal Server Error';
+    err.statusCode = err.statusCode || 500;
+    if (config.env === 'dev') {
+        return devErrors(err, res);
+    }
+    else {
+        let error = {...err,message:err.message}
+        if (err.name === 'CastError') {
+            error = castErrorHandler(error)
+        }
+        if (err.code === 11000) {
+            console.log({err})
+            error = duplicateKeyErrorHandler(error)
+        }
+        return prodErrors(error, res)
+    }
 }
 
 module.exports = {
-    catchAsync,
     errorHandler
 }
