55868eb64049f6f39a12ca87821957244f08b58c
Gilbert Andanje <gilbert@nathandigital.com>
2024-03-10 03:34:57 +0300

    feat: add boiler-plate to each service

---
+++ cartService/utilities/catchAsync.js
@@ -0,0 +1,9 @@
+const catchAsync = (fn) => (req, res, next) => {
+    return Promise.resolve(fn(req, res, next)).catch((err) => {
+        next(err)
+    })
+}
+
+module.exports = {
+    catchAsync
+}
