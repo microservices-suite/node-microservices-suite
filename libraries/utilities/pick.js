d330f588b8f8ea2646a7198453d9d4295ab5e83b
Gilbert Andanje <gilbert@nathandigital.com>
2024-03-12 00:44:50 +0300

    feat: add reusable monorepo library

---
+++ libraries/utilities/pick.js
@@ -0,0 +1,14 @@
+const pick = (object, keys) => {
+  const validSchema = keys.reduce((obj, key) => {
+    if (object && object.hasOwnProperty(key)) {
+      obj[key] = object[key];
+    }
+    return obj;
+  }, {});
+  return validSchema;
+};
+
+
+module.exports = {
+    pick
+}
---
+++ libraries/validations/index.js
@@ -0,0 +1,7 @@
+const getUser = require('./getUser')
+const createUser = require('./createUser')
+
+module.exports = {
+    getUser,
+    createUser
+}
