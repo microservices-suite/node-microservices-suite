f259e24c9bd85421e8e124753fe38d07730c90b0
Gilbert Andanje <gilbert@nathandigital.com>
2024-03-31 16:50:24 +0300

    feat: migrate to yarn workspaces

--- microservices/user-service/index.js
+++ microservices/user-service/index.js
@@ -1,53 +1,59 @@
+const http = require('http');
+const express = require('express');
 const mongoose = require('mongoose');
-const {config} = require('../libraries/config/config');
-const http =  require('http')
-const {getUsers} = require('./src/services/getUsers')
-const {createUser} = require('./src/services/createUser')
-const {getUser} = require('./src/services/getUser')
-const {errorHandler} = require('../libraries/errors/errors.handler')
-const validations = require('../libraries/validations')
-const {validate} = require('../libraries/utilities/validate')
-const {APIError} =  require('../libraries/utilities/APIError')
-const morgan = require('./src/config/morgan')
-const {routes} = require('./src/routes')
-// const app = require('./src/app')
-const express = require('express')
+const { config, morgan, logger } = require('@microservices_suite/config');
+const { errorHandler } = require('@microservices_suite/errors');
+const { getUserValidation, createUserValidation } = require('@microservices_suite/validations');
+const { validate, APIError } = require('@microservices_suite/utilities');
+const { getUsers, createUser, getUser } = require('./src/services');
+const { router } = require('./src/routes');
+// const app = require('./src/app');
 
-mongoose.connect(`mongodb://mongodb:27017`).then(()=>{
-    logger.info(`successfully connected to mongoDB: ${config.db}`);
-}).catch(err=>{
-  logger.error(`failed to connect to mongo. exiting...${err.message}`)
-  process.exit(0)})
-const app = express()
-app.use(express.json())
-app.get('/',(req,res)=>{
-  res.json({messae:'hello from microservices_suite'})
-})
-const server = http.createServer(app)
+mongoose.connect(config.db).then(() => {
+  logger.info(`successfully connected to mongoDB: ${config.db}`);
+}).catch(err => {
+  logger.error(`failed to connect to mongo. exiting...${err.message}`);
+  process.exit(0);
+});
 
+const app = express();
 
-server.on('error',(err)=>{
-  logger.error(err)
-  if(err.code === 'EADDRINUSE'){
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.json({ messae: 'hello from microservices_suite' });
+});
+
+const server = http.createServer(app);
+
+server.on('error', (err) => {
+  logger.error(err);
+  if (err.code === 'EADDRINUSE') {
     logger.error('Address already in use, retrying...');
-    setTimeout(()=>{
+    setTimeout(() => {
       server.close();
-      server.listen(config.port,'localhost')
-    },1000)
-    errorHandler(err)
-  }  
+      server.listen(config.port, 'localhost');
+    }, 1000);
+    errorHandler(err);
+  }
 })
-server.listen(config.port,()=>{
-  logger.info(`http server connected: ${config.port}`)
-})
-app.use(morgan.errorHandler)
-app.use(morgan.successHandler)
-app.use(routes)
+
+server.listen(config.port, () => {
+  logger.info(`http server connected: ${config.port}`);
+});
+
+app.use(morgan.errorHandler);
+
+app.use(morgan.successHandler);
+
+app.use('/api/v1', router);
+
 // global error handler should come after all other middlewares
-app.all('*',(req,res,next)=>{
-  const err =  new APIError(404,`requested resource not found on server: ${req.originalUrl}`)
-  next(err)
-})
-app.use(errorHandler)
+app.all('*', (req, res, next) => {
+  const err = new APIError(404, `requested resource not found on server: ${req.originalUrl}`);
+  next(err);
+});
+
+app.use(errorHandler);
 
 
--- microservices/user-service/src/controllers.js
+++ microservices/user-service/src/controllers.js
@@ -1,22 +1,20 @@
-const { catchAsync } = require('../../libraries/utilities/catchAsync')
-const services = require('./service')
+const services = require('./services')
+const { catchAsync, APIError } = require('@microservices_suite/utilities')
 
-const createUser = catchAsync(async (req,res)=>{
-    const {user} = await services.createUser(req.body)
-    res.status(201).json({data:user})
+const createUser = catchAsync(async (req, res) => {
+    const { user } = await services.createUser(req.body)
+    res.status(201).json({ data: user })
 })
-
-const getUsers = catchAsync(async(req,res)=>{
-    const {users} = await services.getUsers()
-    res.status(200).json({data:users})
+const getUsers = catchAsync(async (req, res) => {
+    const { users } = await services.getUsers()
+    res.status(200).json({ data: users })
 })
-
-const getUser = catchAsync(async (req,res)=>{
-    const {user} = await services.getUserById(req.params.id)
-    if(!user){
-        throw new APIError(404,'user not found')
-    } 
-    res.status(200).json({data:user})
+const getUser = catchAsync(async (req, res) => {
+    const { user } = await services.getUserById(req.params.id)
+    if (!user) {
+        throw new APIError(404, 'user not found')
+    }
+    res.status(200).json({ data: user })
 })
 
 module.exports = {
