a0020d37b421d4d10a40a56d760573a776512f25
Gilbert Andanje <gilbertandanje@gmail.com>
2024-03-31 16:54:27 +0300

    feat: introduce organization scoped libraries

---
+++ shared/errors/index.js
@@ -0,0 +1,1 @@
+module.exports.errorHandler = require('./errors.handler')
---
+++ shared/utilities/validate.js
@@ -0,0 +1,19 @@
+const Joi = require('joi')
+const pick = require('./pick')
+
+const validate = (schema) => (req, res, next) => {
+    const validSchema = pick(schema, ['params', 'body', 'query'])
+    const filterdRequest = pick(req, Object.keys(validSchema))
+    const { error, warning, value } = Joi.compile(validSchema).preferences({ abortEarly: false, convert: true, allowUnknown: true }).validate(filterdRequest)
+    if (error) {
+        let processedErrors = {};
+        error.details.forEach(detail => {
+            const fieldName = detail.context.label.split('.')[1]; // Extract field name from label
+            processedErrors[fieldName] = detail.message.replace(detail.context.label, fieldName); // Replace label with field name
+        });
+        next(processedErrors)
+    }
+    next()
+}
+
+module.exports = validate
