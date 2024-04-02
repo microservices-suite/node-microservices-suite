8e355e7faf26489ba23fc83dce89371d1a1a6c9c
Gilbert Andanje <gilbertandanje@gmail.com>
2024-04-02 21:17:47 +0300

    feat: add readme per package

---
+++ tests/user-service/unit/arrays.test.js
@@ -0,0 +1,12 @@
+const shoppingList = [
+    'diapers',
+    'kleenex',
+    'trash bags',
+    'paper towels',
+    'milk',
+  ];
+  
+  test('the shopping list has milk on it', () => {
+    expect(shoppingList).toContain('milk');
+    expect(new Set(shoppingList)).toContain('milk');
+  });
---
+++ tests/user-service/unit/strings.test.js
@@ -0,0 +1,7 @@
+test('there is no I in team', () => {
+    expect('team').not.toMatch(/I/);
+  });
+  
+  test('but there is a "stop" in Christoph', () => {
+    expect('Christoph').toMatch(/stop/);
+  });
