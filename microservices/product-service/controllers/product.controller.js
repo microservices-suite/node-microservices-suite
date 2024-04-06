0c2fa1fdff34d59fa28bd95d0e2705466581d84a
ddhairya <d_dhairya@yahoo.com>
2024-04-06 18:36:44 +0300

    feat core product initial MCS

---
+++ microservices/product-service/controllers/product.controller.js
@@ -0,0 +1,14 @@
+const httpstatus = require('http-status')
+
+const getAllProduct = async(req, res) => {
+    res.send({ data : []})
+}
+
+const createProduct = async(req, res) => {
+    res.send
+}
+
+module.exports = {
+    getAllProduct,
+    createProduct
+}
