8fae2677be92a0bb83575b4e844e98d7b981099a
ddhairya <d_dhairya@yahoo.com>
2024-04-08 22:25:05 +0300

    feat core supplier initial setup

---
+++ microservices/supplier-service/routes/client/supplier.route.js
@@ -0,0 +1,7 @@
+const express = require('express')
+const router = express.Router()
+const {supllierController} = require('../../controllers')
+
+router.route('/')
+    .get(supllierController.getAllSupplier())
+    .post(supllierController.createSupplier())
