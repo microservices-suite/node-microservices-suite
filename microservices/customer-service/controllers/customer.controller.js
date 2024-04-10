88b261eabe542f331220bfb52c618417fe7c29a2
ddhairya <d_dhairya@yahoo.com>
2024-04-10 22:17:24 +0300

    feat core adding RabbitMQ intial setup between cust and sup

--- microservices/customer-service/controllers/customer.controller.js
+++ microservices/customer-service/controllers/customer.controller.js
@@ -1,5 +1,11 @@
+const { publishMessage, subscribeMessage} = require('../utils/index')
+const {PRODUCT_BINDING_KEY, CUSTOMER_BINDING_KEY, SUPPLIER_BINDING_KEY} = require('../config/index')
+
 const getAllCustomer = async(req, res) => {
     try {        
+        // subscribeMessage(req.rabbitMQChannel,"")
+        publishMessage(req.rabbitMQChannel, SUPPLIER_BINDING_KEY, JSON.stringify({ "name" : "customer"}))   
+
         res.status(200).send({ data: ["This is getAllCustomer"]})
     } catch (error) {
         console.log(error)
--- microservices/supplier-service/index.js
+++ microservices/supplier-service/index.js
@@ -1,11 +1,25 @@
 const express = require('express')
 const app = express()
 const routes = require('./routes')
+const {createRMQChannel, subscribeMessage} = require('./utlis')
+const {rabbitMQMiddleware} = require('./middlewares/rabbitmq')
+
 
 app.use(express.json())
 
-app.use('/',routes)
 
-app.listen(9001, ()=>{
-    console.log('Server listen to port : 9001')
-})
+async function startServer() {
+    try {
+        const channel = await createRMQChannel()
+        subscribeMessage(channel,"")
+        app.use(rabbitMQMiddleware(channel))
+        app.use('/', routes)
+        app.listen(9001, () => {
+            console.log('Server listening on port: 9001')
+        })
+    } catch (error) {
+        console.error('Error starting server:', error)
+    }
+}
+
+startServer()
