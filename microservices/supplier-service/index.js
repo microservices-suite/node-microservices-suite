386d5581b2f36f4aa4aae01933c0ca419e6a575d
ddhairya <d_dhairya@yahoo.com>
2024-04-13 11:20:27 +0300

    bug supplier ading shared lib

--- microservices/supplier-service/index.js
+++ microservices/supplier-service/index.js
@@ -3,6 +3,7 @@ const app = express()
 const routes = require('./routes')
 const {createRMQChannel, subscribeMessage} = require('./utlis')
 const {rabbitMQMiddleware} = require('./middlewares/rabbitmq')
+const { config, morgan, logger } = require('@microservices-suite/config');
 
 
 app.use(express.json())
@@ -10,9 +11,12 @@ app.use(express.json())
 
 async function startServer() {
     try {
+        console.log(process.env.NODE_ENV)
         const channel = await createRMQChannel()
         subscribeMessage(channel,"")
         app.use(rabbitMQMiddleware(channel))
+        app.use(morgan.errorHandler)
+        app.use(morgan.successHandler)
         app.use('/', routes)
         app.listen(9001, () => {
             console.log('Server listening on port: 9001')
