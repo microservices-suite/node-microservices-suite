c28b60ab9fb3ec99c7e8251a88ffd9e6f2345059
Gilbert Andanje <gilbertandanje@gmail.com>
2024-05-01 23:02:50 +0300

    refactor: update env to use docker containers

--- microservices/product-service/ecosystem.config.js
+++ microservices/product-service/ecosystem.config.js
@@ -8,12 +8,12 @@ module.exports = {
       instances:4,
       env_production: {
         NODE_ENV: "prod",
-        DATABASE_URL:"mongodb://locahost:27017/payment_service_db_dev",
+        DATABASE_URL:"mongodb://mongodb:27017/payment_service_db_dev",
         PORT:9002
      },
      env_development: {
       NODE_ENV: "dev",
-      DATABASE_URL:"mongodb://locahost:27017/payment_service_db_dev",
+      DATABASE_URL:"mongodb://mongodb:27017/payment_service_db_dev",
       PORT:9002
    }
     }]
