365576d742eaa2bb50967b9c5dd0416cafe728d5
Gilbert Andanje <gilbertandanje@gmail.com>
2024-06-19 09:00:26 +0300

    fix: gateway resolution

--- .suite-cli/cli/scripts/assets/nginxContent.asset.js
+++ .suite-cli/cli/scripts/assets/nginxContent.asset.js
@@ -1,21 +1,24 @@
 module.exports = ({ services }) => {
-    const upstreams = services.map(service => `
-  upstream ${service.name.toLowerCase().replace(/\s+/g, '-')}_upstream {
-      server ${service.name.toLowerCase().replace(/\s+/g, '-')}:${service.port};
+  const upstreams = services.map(service => `
+upstream ${service.name.toLowerCase().replace(/\s+/g, '-')}_upstream {
+  server ${service.name.toLowerCase().replace(/\s+/g, '-')}:${service.port};
+}`).join('\n');
+
+  const serverBlocks = services.map(service => `
+  location /api/v1/${service.name.toLowerCase().replace(/\s+/g, '-')}s {
+      proxy_pass http://${service.name.toLowerCase().replace(/\s+/g, '-')}_upstream;
+      proxy_set_header Host $host;
+      proxy_set_header X-Real-IP $remote_addr;
+      proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+      proxy_set_header X-Forwarded-Proto $scheme;
   }`).join('\n');
 
-    const serverBlocks = services.map(service => `
-      location /${service.name.toLowerCase().replace(/\s+/g, '-')}/ {
-          proxy_pass http://${service.name.toLowerCase().replace(/\s+/g, '-')}_upstream;
-      }`).join('\n');
+  return `
+${upstreams}
 
-    return `http {
-  ${upstreams}
-  
-      server {
-          listen 80;
-  
-  ${serverBlocks}
-      }
-  }`;
-};  
+server {
+  listen 80;
+
+${serverBlocks}
+}`;
+};
