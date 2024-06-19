8e604db01700c3cb213e6a7f629c72c05be52326
Gilbert Andanje <gilbertandanje@gmail.com>
2024-06-19 04:59:11 +0300

    refactor: isolate dbs per service

--- .suite-cli/cli/scripts/assets/ecosystemContent.asset.js
+++ .suite-cli/cli/scripts/assets/ecosystemContent.asset.js
@@ -9,12 +9,16 @@ module.exports = {
       instances:4,
       env_production: {
         NODE_ENV: "prod",
-        DATABASE_URL:"mongodb://mongodb:27017/${answers.service_name}",
+        DATABASE_URL=mongodb://mongodb:27017/${answers.project_base.slice(1)}_${answers.service_name}_proddb
+        EXCHANGE=@monorepo
+        AMQP_HOST=amqp://rabbitmq:5672
         PORT:${answers.port}
      },
      env_development: {
       NODE_ENV: "dev",
-      DATABASE_URL:"mongodb://mongodb:27017/${answers.service_name}",
+      DATABASE_URL=mongodb://mongodb:27017/${answers.project_base.slice(1)}_${answers.service_name}_devdb
+      EXCHANGE=@monorepo
+      AMQP_HOST=amqp://rabbitmq:5672
       PORT:${answers.port}
    }
     }]
