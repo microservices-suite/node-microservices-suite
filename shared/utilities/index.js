1a46bc061ffa8d21f93fa20694f96fc6c696fbdd
Gilbert Andanje <gilbertandanje@gmail.com>
2024-09-25 00:24:59 +0300

    refactor: rename catchAsync

--- shared/utilities/index.js
+++ shared/utilities/index.js
@@ -1,4 +1,4 @@
 module.exports.APIError = require('./APIError')
-module.exports.catchAsync = require('./catchAsync')
+module.exports.asyncErrorHandler = require('./asyncErrorHandler')
 module.exports.pick = require('./pick')
 module.exports.validate = require('./validate')
