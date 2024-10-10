0875951564b98bae537c1c496d9f766c0a62d1b7
Gilbert Andanje <gilbertandanje@gmail.com>
2024-10-10 13:30:15 +0300

    fix: correct json string value

--- microservices/user/ecosystem.config.js
+++ microservices/user/ecosystem.config.js
@@ -9,16 +9,16 @@ module.exports = {
       instances:4,
       env_production: {
         NODE_ENV: "prod",
-        DATABASE_URL="mongodb://mongodb:27017/microservices-suite_user_proddb",
-        EXCHANGE="@monorepo",
-        AMQP_HOST="amqp://rabbitmq:5672",
+        DATABASE_URL:"mongodb://mongodb:27017/microservices-suite_user_proddb",
+        EXCHANGE:"@microservices-suite",
+        AMQP_HOST:"amqp://rabbitmq:5672",
         PORT:9011
      },
      env_development: {
       NODE_ENV: "dev",
-      DATABASE_URL="mongodb://mongodb:27017/microservices-suite_user_devdb",
-      EXCHANGE="@monorepo",
-      AMQP_HOST="amqp://rabbitmq:5672",
+      DATABASE_URL:"mongodb://mongodb:27017/microservices-suite_user_devdb",
+      EXCHANGE:"@microservices-suite",
+      AMQP_HOST:"amqp://rabbitmq:5672",
       PORT:9011
    }
     }]
